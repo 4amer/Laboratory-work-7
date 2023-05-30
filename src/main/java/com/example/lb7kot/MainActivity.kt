91e8bd3c766f25561c9a333b4a55fa8aba103a02
4amer <ggggamer23080032@gmail.com>
2023-05-31 00:01:31 +0300

    first commit

---
+++ src/main/java/com/example/lb7kot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lb7kot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
